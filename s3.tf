af6894475e347201058e35b24348044b2aef44af
Roman <roman.suslov.18@gmail.com>
2019-02-15 15:11:04 +0000

    next 3

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "terraform-state" {
     bucket = "terraform-state-cd1235d6yjd"
     acl = "private"
+    force_destroy = true    
     versioning {
      enabled = true
     }
